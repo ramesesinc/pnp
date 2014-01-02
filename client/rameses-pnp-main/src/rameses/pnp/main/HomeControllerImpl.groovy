3da45ffa9b56cbeebb2cf8c7644077b111c17049
worgflores <worgflores@gmail.com>
2014-01-02 16:50:55 +0800

    added rameses-pnp-main

---
+++ client/rameses-pnp-main/src/rameses/pnp/main/HomeControllerImpl.groovy
@@ -0,0 +1,27 @@
+package rameses.pnp.main;
+
+import com.rameses.rcp.common.*;
+import com.rameses.rcp.annotations.*;
+import com.rameses.rcp.framework.*;
+import com.rameses.util.*;
+import com.rameses.osiris2.*;
+import com.rameses.osiris2.client.*;
+
+public class HomeControllerImpl {
+
+    def formActions = [];
+    
+    void init(){
+        
+        def actionProvider = ClientContext.currentContext.actionProvider;
+        formActions = actionProvider.lookupActions('home.action');
+        formActions.each{ 
+            if (!it.icon) it.icon = 'images/home-icon.png';
+
+            def target = it.properties.target+'';
+            if (!target.matches('window|popup|process|_window|_popup|_process')) 
+            	it.properties.target = 'window'; 
+        }
+    }
+
+}
