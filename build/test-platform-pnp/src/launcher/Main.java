02af56224e330bcc4c425d7190e3add437f5850e
worgflores <worgflores@gmail.com>
2014-01-02 16:49:34 +0800

    added test-platform-pnp

---
+++ build/test-platform-pnp/src/launcher/Main.java
@@ -0,0 +1,36 @@
+package launcher;
+
+import com.rameses.osiris2.test.OsirisTestPlatform;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.UIManager;
+
+public class Main 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        try { 
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); 
+        } catch(Exception e){;} 
+        
+        Map env = new HashMap();
+        env.put("app.debugMode", "true");         
+        env.put("app.cluster", "osiris3");
+        env.put("app.context", "epims");
+        env.put("app.host", "localhost:8070");
+        env.put("readTimeout", "30000"); 
+        
+        Map profile = new HashMap();
+        profile.put("CLIENTTYPE", "desktop");
+        profile.put("USERID", "sa");
+        profile.put("USER", "sa");
+        profile.put("ORGID", "6000");
+        profile.put("ORGNAME", "CEBU CITY");
+        
+        Map roles = new HashMap();
+        roles.put("ADMIN.SYSADMIN", null);
+        roles.put("POLICE.DATAMGMT_AUTHOR", null);
+        roles.put("POLICE.ENCODER", null);
+        OsirisTestPlatform.runTest(env, roles, profile); 
+    }    
+}
